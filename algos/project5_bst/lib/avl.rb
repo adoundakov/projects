fb3c53166829606cc5f0fb986605b068caa43302
Alex Doundakov <adoundakov@gmail.com>
2016-12-05 10:33:13 -0800

    more avl progress

--- algos/project5_bst/lib/avl.rb
+++ algos/project5_bst/lib/avl.rb
@@ -13,9 +13,10 @@ class AVLTree
     @root = nil
   end
 
-  def insert(value)
-    @root = AVLTree.insert!(@root, value)
-  end
+  # Old insert method
+  # def insert(value)
+  #   @root = AVLTree.insert!(@root, value)
+  # end
 
   def self.insert!(node, value)
     return AVLTreeNode.new(value) unless node
@@ -51,5 +52,69 @@ class AVLTree
     other_node.link[dir] = root
 
     other_node # will define new root
+  end
+
+  def self.adjust_balance!(root, dir, bal)
+    other_dir = dir.zero? ? 1 : 0
+
+    n = root.link[dir] # root in direction
+    nn = n.link[other_dir] # root's direction child in other direction
+
+    if nn.balance.zero?
+      root.balance = 0
+      n.balance = 0
+    elsif nn.balance == bal
+      root.balance = -bal
+      n.balance = 0
+    else
+      root.balance = 0
+      n.balance = bal
+    end
+
+    nn.balance = 0
+  end
+
+  def self.insert_balance!(root, dir)
+    other_dir = dir.zero? ? 1 : 0
+    bal = dir.zero? ? -1 : +1
+
+    n = root.link[dir]
+
+    if n.balance == bal
+      root.balance = 0
+      n.balance = 0
+      root = AVLTree.single_rotation!(root, other_dir)
+    else
+      AVLTree.adjust_balance!(root, dir, bal)
+      root = AVLTree.double_rotation!(root, other_dir)
+    end
+
+    root
+  end
+
+  def self.insert!(node, value, done)
+    return [AVLTreeNode.new(value), false] unless node
+
+    dir = value < node.value ? 0 : 1
+
+    node.link[dir], done = AVLTree.insert!(node.link[dir], value, done)
+
+    unless done
+      node.balance += (dir.zero? ? -1: +1)
+
+      if node.balance.zero?
+        done = true
+      elsif node.balance.abs > 1
+        node = AVLTree.insert_balance!(node, dir)
+        done = true
+      end
+    end
+
+    [node, done]
+  end
+
+  def insert(value)
+    done = false
+    @root, done = AVLTree.insert!(@root, value, done)
   end
 end
