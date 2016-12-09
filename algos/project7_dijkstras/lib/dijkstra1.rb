58424c92b8fe2f11207416108e9d486a31e4f5b5
Alex Doundakov <adoundakov@gmail.com>
2016-12-09 15:56:00 -0800

    add soln from lecture

--- algos/project7_dijkstras/lib/dijkstra1.rb
+++ algos/project7_dijkstras/lib/dijkstra1.rb
@@ -3,3 +3,12 @@ require_relative 'graph'
 # O(|V|**2 + |E|).
 def dijkstra1(source)
 end
+
+# general approach
+
+# for starting node
+#   make list of visited nodes, starts with just src
+#   make list of costs to reach nodes, start at -1 or inf
+#   visit all unvisited nodes possible, updating costs
+#   select lowest cost path, add verted to visited, repeat
+#   once no more possible paths, return shortest path
