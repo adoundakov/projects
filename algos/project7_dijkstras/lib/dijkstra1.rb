d322236af009cd59e9233f51dee6132549cd994b
Alex Doundakov <adoundakov@gmail.com>
2016-12-12 12:10:14 -0800

    algo proj 7: add dj1 and priorityMap code

--- algos/project7_dijkstras/lib/dijkstra1.rb
+++ algos/project7_dijkstras/lib/dijkstra1.rb
@@ -2,6 +2,25 @@ require_relative 'graph'
 
 # O(|V|**2 + |E|).
 def dijkstra1(source)
+  best_paths = {}
+  potential_paths = { source => { cost: 0, last_edge: nil } }
+  until potential_paths.empty?
+    path = potential_paths.min_by { |_, v| v[:cost] }
+    vertex = path[0]
+    best_paths[vertex] = path[1]
+    potential_paths.delete(vertex)
+    curr_path_cost = best_paths[vertex][:cost]
+    vertex.out_edges.each do |e|
+      to_vertex = e.to_vertex
+      next_path_cost = curr_path_cost + e.cost
+      next unless best_paths[to_vertex].nil?
+      next if potential_paths[to_vertex] &&
+              potential_paths[to_vertex][:cost] <= next_path_cost
+      potential_paths[to_vertex] = { cost: next_path_cost, last_edge: e }
+    end
+  end
+
+  best_paths
 end
 
 # general approach
--- algos/project7_dijkstras/lib/priority_map.rb
+++ algos/project7_dijkstras/lib/priority_map.rb
@@ -2,32 +2,50 @@ require_relative 'heap2'
 
 class PriorityMap
   def initialize(&prc)
+    @map = {}
+    @queue = BinaryMinHeap.new { |k1, k2| prc.call(@map[k1], @map[k2]) }
   end
 
   def [](key)
+    return nil unless @map[key]
+    @map[key]
   end
 
   def []=(key, value)
+    @map[key] ? update(key, value) : insert(key, value)
   end
 
   def count
+    @map.count
   end
 
   def empty?
+    count.zero?
   end
 
   def extract
+    key = @queue.extract
+    val = @map.delete(key)
+    [key, val]
   end
 
-  def has_key?(key)
+  def has_key?(key) # should rename to #key? instead
+    !@map[key].nil?
   end
 
   protected
+
   attr_accessor :map, :queue
 
   def insert(key, value)
+    @map[key] = value
+    @queue.push(key)
   end
 
   def update(key, value)
+    raise "non-existent key" unless has_key?(key)
+    @map[key] = value
+    @queue.reduce!(key)
+    nil
   end
 end
