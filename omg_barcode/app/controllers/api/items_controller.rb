0c5b8b9d73e96b55b61e85c4b41992155b1e5f9f
Alex Doundakov <adoundakov@gmail.com>
2017-01-26 23:04:50 -0800

    add controller skeleton, fill out items controller

---
+++ omg_barcode/app/controllers/api/items_controller.rb
@@ -0,0 +1,22 @@
+class Api::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new(item_params)
+    if @item.save
+      success = ['Item Saved Successfully']
+      render json: success, status: 200
+    else
+      errors = @item.errors.full_messages
+      render json: errors, status: 422
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :upc)
+  end
+end
