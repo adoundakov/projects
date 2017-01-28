c708749ad1f528de980717f787abffb9ba591eb8
adoundakov <adoundakov@gmail.com>
2017-01-28 11:41:53 -0800

    redo quicksort project

--- algos/project4_quicksort/spec/quick_sort_spec.rb
+++ algos/project4_quicksort/spec/quick_sort_spec.rb
@@ -1,8 +1,8 @@
-require "quick_sort"
+require 'quick_sort'
 
 describe QuickSort do
-  describe "partition" do
-    it "partitions the whole array properly" do
+  describe 'partition' do
+    it 'partitions the whole array properly' do
       arr = [3, 1, 5, 2, 4]
       pivot_idx = QuickSort.partition(arr, 0, 5)
 
@@ -10,7 +10,7 @@ describe QuickSort do
       expect(pivot_idx).to eq(2)
     end
 
-    it "partitions a portion of the array" do
+    it 'partitions a portion of the array' do
       arr = [4, 3, 2, 1, 7, 5, 8, 6]
       pivot_idx = QuickSort.partition(arr, 4, 4)
       # Should not touch left half of the array
@@ -19,14 +19,14 @@ describe QuickSort do
     end
   end
 
-  describe "sort2!" do
-    it "sorts an array" do
+  describe 'sort2!' do
+    it 'sorts an array' do
       arr = [5, 3, 4, 2, 1, 6]
       QuickSort.sort2!(arr)
       expect(arr).to eq([1, 2, 3, 4, 5, 6])
     end
 
-    it "makes the right number of comparisons (good case)" do
+    it 'makes the right number of comparisons (good case)' do
       arr = [4, 2, 1, 3, 6, 5, 7]
 
       num_comparisons = 0
@@ -38,7 +38,7 @@ describe QuickSort do
       expect(num_comparisons).to eq(10)
     end
 
-    it "makes the right number of comparisons (worst case)" do
+    it 'makes the right number of comparisons (worst case)' do
       arr = [1, 2, 3, 4, 5]
 
       num_comparisons = 0
