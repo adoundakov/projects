a40cdf4a5f1d01df4429ab28d19a9b60faa4b075
adoundakov <adoundakov@gmail.com>
2017-01-28 13:56:00 -0800

    redo bst project, add skeleton and solutions folder

---
+++ algos/project5_bst/solutions/avl.rb
@@ -0,0 +1,32 @@
+class AVLTreeNode
+  attr_accessor :link, :value
+
+  def initialize
+  end
+end
+
+class AVLTree
+  def initialize
+  end
+
+  def self.insert!(node, value)
+  end
+
+  def self.single_rotation!(root, dir)
+  end
+
+  def self.double_rotation!(root, dir)
+  end
+
+  def self.adjust_balance!(root, dir, bal)
+  end
+
+  def self.insert_balance!(root, dir)
+  end
+
+  def self.insert!(node, value, done)
+  end
+
+  def insert(value)
+  end
+end
