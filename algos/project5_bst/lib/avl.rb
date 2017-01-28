2595c96ce1fb18b42b31f506d4ae9a2561ad8ee1
adoundakov <adoundakov@gmail.com>
2017-01-28 13:57:34 -0800

    bst project: switch solutions and skeleton

--- algos/project5_bst/lib/avl.rb
+++ algos/project5_bst/lib/avl.rb
@@ -2,119 +2,31 @@ class AVLTreeNode
   attr_accessor :link, :value
 
   def initialize
-    @value = value
-    @link = [nil, nil]
-    @balance = 0
   end
 end
 
 class AVLTree
   def initialize
-    @root = nil
   end
 
-  # Old insert method
-  # def insert(value)
-  #   @root = AVLTree.insert!(@root, value)
-  # end
-
   def self.insert!(node, value)
-    return AVLTreeNode.new(value) unless node
-
-    # where to go? less = left, greater = right
-    dir = value < node.value ? 0 : 1
-
-    node.link[dir] = insert!(node.link[dir], value)
-
-    node
   end
 
   def self.single_rotation!(root, dir)
-    other_dir = dir == 0 ? 1 : 0
-    other_node = root.link[other_dir]
-
-    root.link[other_dir] = other_node.link[dir]
-    other_node.link[dir] = root
-
-    other_node # will define new root
   end
 
   def self.double_rotation!(root, dir)
-    other_dir = dir == 0 ? 1 : 0
-    other_node = root.link[other_dir].link[dir]
-
-    root.link[other_dir].link[dir] = other_node.link[other_dir]
-    other_node.link[other_dir] = root.link[other_dir]
-    root.link[other_dir] = other_node
-
-    other_node = root.link[other_dir]
-    root.link[other_dir] = other_node.link[dir]
-    other_node.link[dir] = root
-
-    other_node # will define new root
   end
 
   def self.adjust_balance!(root, dir, bal)
-    other_dir = dir.zero? ? 1 : 0
-
-    n = root.link[dir] # root in direction
-    nn = n.link[other_dir] # root's direction child in other direction
-
-    if nn.balance.zero?
-      root.balance = 0
-      n.balance = 0
-    elsif nn.balance == bal
-      root.balance = -bal
-      n.balance = 0
-    else
-      root.balance = 0
-      n.balance = bal
-    end
-
-    nn.balance = 0
   end
 
   def self.insert_balance!(root, dir)
-    other_dir = dir.zero? ? 1 : 0
-    bal = dir.zero? ? -1 : +1
-
-    n = root.link[dir]
-
-    if n.balance == bal
-      root.balance = 0
-      n.balance = 0
-      root = AVLTree.single_rotation!(root, other_dir)
-    else
-      AVLTree.adjust_balance!(root, dir, bal)
-      root = AVLTree.double_rotation!(root, other_dir)
-    end
-
-    root
   end
 
   def self.insert!(node, value, done)
-    return [AVLTreeNode.new(value), false] unless node
-
-    dir = value < node.value ? 0 : 1
-
-    node.link[dir], done = AVLTree.insert!(node.link[dir], value, done)
-
-    unless done
-      node.balance += (dir.zero? ? -1: +1)
-
-      if node.balance.zero?
-        done = true
-      elsif node.balance.abs > 1
-        node = AVLTree.insert_balance!(node, dir)
-        done = true
-      end
-    end
-
-    [node, done]
   end
 
   def insert(value)
-    done = false
-    @root, done = AVLTree.insert!(@root, value, done)
   end
 end
--- algos/project5_bst/lib/bst.rb
+++ algos/project5_bst/lib/bst.rb
@@ -1,150 +1,72 @@
-class BSTNode
+class BSTNode # :nodoc:
   attr_accessor :left, :right
   attr_reader :value
 
   def initialize(value)
-    @value = value
-    @left = nil
-    @right = nil
   end
 
   def children
-    [@left, @right]
   end
 end
 
 class BinarySearchTree
   def initialize
-    @root = nil
   end
 
   def insert(value)
-    @root ? BinarySearchTree.insert!(@root, value) : @root = BSTNode.new(value)
   end
 
   def find(value)
-    BinarySearchTree.find!(@root, value)
   end
 
   def inorder
-    BinarySearchTree.inorder!(@root)
   end
 
   def postorder
-    BinarySearchTree.postorder!(@root)
   end
 
   def preorder
-    BinarySearchTree.preorder!(@root)
   end
 
   def height
-    BinarySearchTree.height!(@root)
   end
 
   def min
-    BinarySearchTree.min(@root)
   end
 
   def max
-    BinarySearchTree.max(@root)
   end
 
   def delete(value)
-    BinarySearchTree.delete!(@root, value)
   end
 
   def self.insert!(node, value)
-    return node = BSTNode.new(value) unless node
-
-    if node.value >= value
-      node.left = insert!(node.left, value)
-    else
-      node.right = insert!(node.right, value)
-    end
-    node
   end
 
   def self.find!(node, value)
-    return nil unless node
-    return node if node.value == value
-
-    if node.value < value
-      find!(node.right, value)
-    else
-      find!(node.left, value)
-    end
   end
 
   def self.preorder!(node)
-    return [] if node.nil?
-    values = []
-    values << node.value
-    values.concat(preorder!(node.left)) if node.left
-    values.concat(preorder!(node.right)) if node.right
-    values
   end
 
   def self.inorder!(node)
-    return [] if node.nil?
-    values = []
-    values.concat(inorder!(node.left)) if node.left
-    values << node.value
-    values.concat(inorder!(node.right)) if node.right
-    values
   end
 
   def self.postorder!(node)
-    return [] if node.nil?
-    values = []
-    values.concat(postorder!(node.left)) if node.left
-    values.concat(postorder!(node.right)) if node.right
-    values << node.value
-    values
   end
 
   def self.height!(node)
-    return -1 unless node
-    [height!(node.left), height!(node.right)].max + 1
   end
 
   def self.max(node)
-    return nil unless node
-    return node if node.right.nil?
-    max(node.right)
   end
 
   def self.min(node)
-    return nil unless node
-    return node if node.left.nil?
-    min(node.left)
   end
 
   def self.delete_min!(node)
-    return nil unless node
-    return node.right if node.left.nil?
-    node.left = delete_min!(node.left)
-    node
   end
 
   def self.delete!(node, value)
-    return nil unless node
-
-    if value < node.value
-      node.left = delete!(node.left, value)
-    elsif value > node.value
-      node.right = delete!(node.right, value)
-    else
-      # if only single child
-      return node.left unless node.right
-      return node.right unless node.left
-      tmp = node
-      # tmp.right.min  why min?? meant min(tmp.right) ??
-      node = min(tmp.right)
-      node.right = delete_min!(tmp.right) # replace old right with min right
-      node.left = tmp.left # replace left with left of original node
-    end
-
-    node
   end
 end
